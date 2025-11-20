8d7bb8103d2e56f026c0968f371fd10f7ebb4879
juliamendespditabira955 <juliaeduarda.mendespdita955@pditabira.com>
2025-11-19 22:34:30 -0300

    Add files via upload

---
+++ Módulo 3/aula2_questao3.py
@@ -0,0 +1,12 @@
+input("------- BEM - VINDO --------")
+idade = int(input("Qual a sua idade ? "))
+anos = idade >=16 or =18 
+print(anos)
+qjogos = int(input("Quantos jogos de tabuleiro você já jogou ?"))
+jogos = qjogos >=3
+print(jogos)
+qvenceu = int(input("Quantas vezes você já ganhou ?"))
+vitorias = qvenceu >=1
+sim_ou_nao = anos = jogos = vitorias
+print("Está apto para entrar no clube de jogos de tabuleiro?")
+print(sim_ou_nao)
