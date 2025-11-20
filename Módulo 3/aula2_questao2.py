3b2284f41f8302ed21453c1644e882b748592d87
juliamendespditabira955 <juliaeduarda.mendespdita955@pditabira.com>
2025-11-19 22:13:39 -0300

    Add files via upload

---
+++ Módulo 3/aula2_questao2.py
@@ -0,0 +1,5 @@
+idjuliana = int(input("Qual a idade de Juliana?"))
+idcris = int(input("Qual a idade de Cris ?"))
+idx = int(input("Qual a idade da terceira ?"))
+podeentrar = idx >= 18 
+print (podeentrar) 
