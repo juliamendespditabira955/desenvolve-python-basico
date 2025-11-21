5683cb47827b625a81e41bd501a12619ee891513
juliamendespditabira955 <juliaeduarda.mendespdita955@pditabira.com>
2025-11-21 15:51:01 -0300

    Add files via upload

---
+++ Módulo 3/aula4_questao2.py
@@ -0,0 +1,12 @@
+input("-------- BEM - VINDO --------")
+avaliacao = int(input("Avalie o último filme que você assistiu em uma escala de 1 a 5:"))
+if avaliacao == 5:
+    print ("Exceente!")
+if avaliacao == 4:
+    print ("Muito Bom !")
+if avaliacao == 3:
+    print("Bom!")
+if avaliacao == 2:
+    print("Regular.")
+if avaliacao == 1:
+    print("Ruim.")
